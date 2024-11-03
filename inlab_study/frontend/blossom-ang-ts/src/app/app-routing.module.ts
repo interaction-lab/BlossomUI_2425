b1a3c298a7ddee39098b528ef42423a45a6b7dbf
sanyachandnikhattar <sckhatta@usc.edu>
2024-11-03 15:18:50 -0800

    fixing app.component.ts, app-routing.module.ts, etc.

--- inlab_study/frontend/blossom-ang-ts/src/app/app-routing.module.ts
+++ inlab_study/frontend/blossom-ang-ts/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = 
-[{ },
-
+[  {path: '', redirectTo: '/landing', pathMatch: 'full' }, //default route --> landing page
+   {path: 'landing', component: LandingComponent }, 
+   {path :'**', redirectTo: '/landing'} //wildcard route for 404
 ];
 
 @NgModule({
