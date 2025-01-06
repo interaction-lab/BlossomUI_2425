730f33da0afd2be9859cd220efbc66ce9a529465
a-oconnell <ao71627@usc.edu>
2025-01-06 15:18:05 -0800

    add LED and sound commands for idle behaviors

--- inlab_study/backend/scripts/study_script.py
+++ inlab_study/backend/scripts/study_script.py
@@ -1,4 +1,25 @@
+import sys
+import board
+import busio
+import random
+import pygame
 
+sys.path.insert(1, '/home/blossom/blossom-public')
+from LED.rpi_led_sequence import pulse_effect, row_traverse, twinkle_effect, color_chase, fade_in_out, all_on_off, rainbow_wave, strobe_effect
+from LED.motor_movements import BlossomController
+
+bc = BlossomController()
+
+# Define some colors
+color_list = [(255, 0, 0),(0, 255, 0),(0, 0, 255),(255, 255, 0),(0, 255, 255),(128, 0, 128)]
+light_seq_list = [pulse_effect, row_traverse, twinkle_effect, color_chase, fade_in_out, all_on_off, rainbow_wave, strobe_effect]
+
+# Create I2C bus object
+i2c = busio.I2C(board.SCL, board.SDA)
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('/home/blossom/Downloads/S72_PURR_1.wav')
 
 # backend/python/button_handlers.py
 def start_handler():
@@ -15,6 +36,11 @@ def end_handler():
 
 def ib_handler():
     print("Performing idle behavior")
+    num = random.randint(0, len(light_seq_list)-1)
+    colorNum = random.randint(0, len(color_list)-1)
+    pygame.mixer.music.play()
+    light_seq_list[num](color_list[colorNum])
+    bc.sigh()
     return "Performing idle behavior"
 
 if __name__ == "__main__":
