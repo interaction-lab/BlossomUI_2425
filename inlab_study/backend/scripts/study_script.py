a4d5b154f020cba9cc9838635e2494af3e52d696
a-oconnell <ao71627@usc.edu>
2025-01-06 15:25:20 -0800

    Update study_script.py

--- inlab_study/backend/scripts/study_script.py
+++ inlab_study/backend/scripts/study_script.py
@@ -1,18 +1,18 @@
 import sys
 import board
 import busio
-import random
+# import random
 import pygame
 
 sys.path.insert(1, '/home/blossom/blossom-public')
-from LED.rpi_led_sequence import pulse_effect, row_traverse, twinkle_effect, color_chase, fade_in_out, all_on_off, rainbow_wave, strobe_effect
-from LED.motor_movements import BlossomController
+# from LED.rpi_led_sequence import pulse_effect, row_traverse, twinkle_effect, color_chase, fade_in_out, all_on_off, rainbow_wave, strobe_effect
+# from LED.motor_movements import BlossomController
 
-bc = BlossomController()
+# bc = BlossomController()
 
 # Define some colors
 color_list = [(255, 0, 0),(0, 255, 0),(0, 0, 255),(255, 255, 0),(0, 255, 255),(128, 0, 128)]
-light_seq_list = [pulse_effect, row_traverse, twinkle_effect, color_chase, fade_in_out, all_on_off, rainbow_wave, strobe_effect]
+# light_seq_list = [pulse_effect, row_traverse, twinkle_effect, color_chase, fade_in_out, all_on_off, rainbow_wave, strobe_effect]
 
 # Create I2C bus object
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -36,11 +36,11 @@ def end_handler():
 
 def ib_handler():
     print("Performing idle behavior")
-    num = random.randint(0, len(light_seq_list)-1)
-    colorNum = random.randint(0, len(color_list)-1)
+    # num = random.randint(0, len(light_seq_list)-1)
+    # colorNum = random.randint(0, len(color_list)-1)
     pygame.mixer.music.play()
-    light_seq_list[num](color_list[colorNum])
-    bc.sigh()
+    # light_seq_list[num](color_list[colorNum])
+    # bc.sigh()
     return "Performing idle behavior"
 
 if __name__ == "__main__":
