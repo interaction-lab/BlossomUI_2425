3cd69b928d689a51e4b418ee24343b4d00cd51cd
a-oconnell <ao71627@usc.edu>
2025-01-26 19:10:06 -0800

    Update study_script.py

--- inlab_study/backend/scripts/study_script.py
+++ inlab_study/backend/scripts/study_script.py
@@ -7,136 +7,7 @@ import random
 import pygame
 import sqlite3
 
-# getting Blossom control code directly ////////////////////////////////////////////////
-sys.path.insert(1, '/home/blossom/blossom-public')
-
-
-# Import light sequences
-from LED.rpi_led_sequence import row_traverse, twinkle_effect, color_chase, fade_in_out, fill, rainbow_wave
-from LED.motor_movements import BlossomController
-
-bc = BlossomController()
-pygame.init()
-pygame.mixer.init()
-
-animal_sound_list = [
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/animal_recordings/cat_meow_16.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/animal_recordings/cat_purr_16.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/animal_recordings/guinea_pig.wav"
-]
-
-hybrid_sound_list = [
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/animal_vocables/S29_MEOW_1.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/animal_vocables/S44_SNEEZE_2.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/animal_vocables/S72_PURR_3.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/animal_vocables/S89_BARK_1.wav"
-]
-
-digital_sound_list = [
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/electronic_noises/Beep4.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/electronic_noises/LIKED_MOMENT_2.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/electronic_noises/S77_FUNCTIONAL_SUCCESS.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/electronic_noises/shutter_2.wav"
-]
-
-vocalizations_sound_list = [
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/human_vocables/No.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/human_vocables/S19_WHAT_THE_HECK_2.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/human_vocables/Yes.wav",
-"/home/blossom/blossom-public/personalization/final_sounds_new_categories/human_vocables/YOURE_WELCOME.wav"
-]
-
-bc = BlossomController()
-
-# Light definitions
-R = (255, 0, 0)
-S = (255, 0, 128)
-M = (255, 0, 255)
-P = (128, 0, 128)
-B = (0, 0, 255)
-C = (0, 255, 255)
-G = (0, 255, 0)
-L = (128, 255, 0)
-Y = (255, 255, 0)
-O = (255, 165, 0)
-
-motor_movements_list = [bc.sigh, bc.idle_gaze, bc.posture_sway]
-
-def perform_random_action_constrained(settings):
-    user_id, brightness, volume, animal_sounds, digital_sounds, hybrid_sounds, \
-    vocalizations, red, rose, magenta, purple, blue, cyan, green, lime, yellow, orange = settings
-
-    valid_sounds = []
-
-    # Add sounds to the list based on the boolean flags
-    if animal_sounds:
-        valid_sounds.extend(animal_sound_list)
-    if digital_sounds:
-        valid_sounds.extend(digital_sound_list)
-    if hybrid_sounds:
-        valid_sounds.extend(hybrid_sound_list)
-    if vocalizations:
-        valid_sounds.extend(vocalizations_sound_list)
-
-    # Ensure there are valid sounds to choose from
-    if valid_sounds:
-        # Randomly select a sound from the valid options
-        random_sound = random.choice(valid_sounds)
-    else:
-        random_sound = None
-
-    valid_colors = []
-
-    if red:
-        valid_colors.append(R)
-    if rose:
-        valid_colors.append(S)
-    if magenta:
-        valid_colors.append(M)
-    if purple:
-        valid_colors.append(P)
-    if blue:
-        valid_colors.append(B)
-    if cyan:
-        valid_colors.append(C)
-    if green:
-        valid_colors.append(G)
-    if lime:
-        valid_colors.append(L)
-    if yellow:
-        valid_colors.append(Y)
-    if orange:
-        valid_colors.append(O)
-
-    # Ensure there are valid colors to choose from
-    if valid_colors:
-        # Randomly select a color from the valid options
-        random_color = random.choice(valid_colors)
-        random_color = tuple(int(c * brightness/100) for c in random_color)
-        print(f"Randomly selected color: {random_color}")
-    else:
-        random_color = None   
-    
-    if random_sound:
-        pygame.mixer.music.load(random_sound)  # Play the sound
-        pygame.mixer.music.play()
-        
-    random.choice(motor_movements_list)()  # Perform the motor movement
-
-    if random_color:
-        random.choice(light_seq_list)(random_color)
-
-def end_of_session():
-    rainbow_wave()
-    success_sound = "/home/blossom/blossom-public/personalization/final_sounds_new_categories/misc/success-fanfare-trumpets.mp3"
-    pygame.mixer.music.load(success_sound) 
-    pygame.mixer.music.play()
-
-def perform_trick_1():
-    rainbow_wave()
-#///////////////////////////////////////////////////
-
-# import blossom_multi_model_signals as blossom
+import blossom_multi_model_signals as blossom
 
 def get_settings(user_id):
     # Connect to the database
@@ -180,20 +51,17 @@ def end_handler():
 def ib_handler(participant_id):
     print("Performing idle behavior")
     settings = get_settings(participant_id)
-    # blossom.perform_random_action_constrained(settings)
-    perform_random_action_constrained(participant_id)
+    blossom.perform_random_action_constrained(settings)
     return "Performing idle behavior"
 
 def session_complete_handler():
     print("Performing end of session celebration")
-    # blossom.end_of_session()
-    end_of_session()
+    blossom.end_of_session()
     return "Performing end of session celebration"
 
 def perform_trick_1():
     print("Performing Trick 1")
-    # blossom.end_of_session()
-    end_of_session()
+    blossom.end_of_session()
     return "Trick 1 performed!"
 
 def perform_trick_2():
